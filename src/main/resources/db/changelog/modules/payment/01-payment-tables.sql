cf371ffc0da9c2811f2688bee676b3c30834900e
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-05-14 19:31:11 -0300

    refactor: change tid column in payment table to NOT NULL
    this colum will be used to store 'in_store_order_id', provided by mercado pago api

--- src/main/resources/db/changelog/modules/payment/01-payment-tables.sql
+++ src/main/resources/db/changelog/modules/payment/01-payment-tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS payments (
     customer_id INT,
     type payment_type_enum NOT NULL,
     expires_in TIMESTAMP NOT NULL,
-    tid VARCHAR(255) UNIQUE,
+    tid VARCHAR(255) UNIQUE NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     qr_code_url VARCHAR(255),
     observations TEXT,
