b3c0fdd05df2aaf208c1effc5d967a21382a214d
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-05-22 22:51:57 -0300

    fix: add mapImageUrl method in CategoryResponseMapper

--- src/main/java/com/soat/fiap/food/core/api/catalog/application/mapper/response/ProductResponseMapper.java
+++ src/main/java/com/soat/fiap/food/core/api/catalog/application/mapper/response/ProductResponseMapper.java
@@ -2,7 +2,10 @@ package com.soat.fiap.food.core.api.catalog.application.mapper.response;
 
 import com.soat.fiap.food.core.api.catalog.application.dto.response.ProductResponse;
 import com.soat.fiap.food.core.api.catalog.domain.model.Product;
+import com.soat.fiap.food.core.api.catalog.domain.vo.ImageUrl;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -13,7 +16,13 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = StockResponseMapper.class)
 public interface ProductResponseMapper {
 
+    @Mapping(source = "imageUrl", target = "imageUrl", qualifiedByName = "mapImageUrlToString")
     ProductResponse toResponse(Product product);
 
     List<ProductResponse> toResponseList(List<Product> products);
+
+    @Named("mapImageUrlToString")
+    default String mapImageUrlToString(ImageUrl imageUrl) {
+        return imageUrl != null ? imageUrl.value() : null;
+    }
 }
