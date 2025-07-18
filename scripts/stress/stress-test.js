d4749f7d09e854174aed6939b8305202d26e4b2c
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-07-18 09:23:43 -0300

    feat: implement stress test with k6 to test api hpa

---
+++ scripts/stress/stress-test.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+  stages: [
+    { duration: '30s', target: 50 },
+    { duration: '1m', target: 50 }, 
+    { duration: '30s', target: 0 },   
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.01'],   
+    http_req_duration: ['p(95)<1000'],
+  },
+};
+
+export default function () {
+  const res = http.get('http://localhost/api');
+  check(res, {
+    'StatusCode é 200': (r) => r.status === 200,
+  });
+  sleep(1);
+}
