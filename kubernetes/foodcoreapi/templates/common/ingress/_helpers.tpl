df16382d0b5dc0c8b7bdc4d5dc6cf10013e2f8f8
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-07-26 14:14:22 -0300

    refactor: adjust requests and limits of all resources

--- kubernetes/foodcoreapi/templates/common/ingress/_helpers.tpl
+++ kubernetes/foodcoreapi/templates/common/ingress/_helpers.tpl
@@ -8,16 +8,19 @@
 
 {{- define "ingress.services" -}}
 - name: {{ printf "%s-service" (include "api.fullname" .) }}
+  namespace: {{ .Values.namespace.api.name }}
   port: {{ .Values.api.ports.port }}
   path: {{ .Values.api.ingress.path }}
   pathType: {{ .Values.api.ingress.pathType }}
-- name: {{ printf "%s-service" (include "kibana.fullname" .) }}
-  port: {{ .Values.kibana.ports.port }}
-  path: {{ .Values.kibana.ingress.path }}
-  pathType: {{ .Values.kibana.ingress.pathType }}
 - name: {{ printf "%s-service" (include "adminer.fullname" .) }}
+  namespace: {{ .Values.namespace.api.name }}
   port: {{ .Values.adminer.ports.port }}
   path: {{ .Values.adminer.ingress.path }}
   pathType: {{ .Values.adminer.ingress.pathType }}
+- name: {{ printf "%s-service" (include "kibana.fullname" .) }}
+  namespace: {{ .Values.namespace.efk.name }}
+  port: {{ .Values.kibana.ports.port }}
+  path: {{ .Values.kibana.ingress.path }}
+  pathType: {{ .Values.kibana.ingress.pathType }}
 {{- end }}
 
--- scripts/stress/stress-test.js
+++ scripts/stress/stress-test.js
@@ -1,22 +1,40 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export let check_failure_rate = new Rate('check_failure_rate');
 
 export let options = {
   stages: [
-    { duration: '30s', target: 50 },
-    { duration: '1m', target: 50 },
+    { duration: '6m', target: 100 },
     { duration: '30s', target: 0 },
   ],
   thresholds: {
-    http_req_failed: ['rate<0.01'],
+    check_failure_rate: ['rate<0.01'],
     http_req_duration: ['p(95)<1000'],
   },
 };
 
 export default function () {
-  const res = http.get('http://foodcoreapi.eastus.cloudapp.azure.com/api');
-  check(res, {
-    'StatusCode é 200': (r) => r.status === 200,
+  const payload = JSON.stringify({
+    email: "ddosattack@email.com",
+    password: "passwdverystrong123"
   });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = http.post('http://foodcoreapi2.eastus.cloudapp.azure.com/api/users/login', payload, params);
+
+  const success = check(res, {
+    'StatusCode é 404': (r) => r.status === 404,
+  });
+
+  // Registra falha se o check falhar
+  check_failure_rate.add(!success);
+
   sleep(1);
 }
