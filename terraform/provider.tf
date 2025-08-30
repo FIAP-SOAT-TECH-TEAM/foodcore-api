348c81bf3eda2a2e716f5e614df4322ef17271f3
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-08-30 15:09:48 -0300

    fix: remove varible from provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -24,7 +24,7 @@ provider "kubernetes" {
 provider "helm" {
   registries = [
     {
-      url      = var.repository_url
+      url      = "oci://${data.azurerm_container_registry.acr.login_server}/helm"
       username = data.azurerm_container_registry.acr.admin_username
       password = data.azurerm_container_registry.acr.admin_password
     }
