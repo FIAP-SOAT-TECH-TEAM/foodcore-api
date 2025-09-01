8c218bd3d454a558d5cb1539353159582fde0d65
Pedro Ferrarezzo <ferrarezzolabs@outlook.com>
2025-09-01 20:40:13 -0300

    feat: create apim foodcore api association with product

--- terraform/modules/apim/main.tf
+++ terraform/modules/apim/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_api_management_api" "apim" {
+resource "azurerm_api_management_api" "foodcoreapi_apim" {
   name                = var.apim_api_name
   resource_group_name = data.terraform_remote_state.infra.outputs.apim_resource_group
   api_management_name = data.terraform_remote_state.infra.outputs.apim_name
@@ -33,4 +33,11 @@ resource "azurerm_api_management_api_policy" "set_backend_api" {
     </outbound>
   </policies>
 XML
+}
+
+resource "azurerm_api_management_product_api" "foodcoreapi_start_product_assoc" {
+  api_name            = azurerm_api_management_api.foodcoreapi_apim.name
+  product_id          = data.terraform_remote_state.infra.outputs.apim_foodcore_start_productid
+  api_management_name = data.terraform_remote_state.infra.outputs.apim_name
+  resource_group_name = data.terraform_remote_state.infra.outputs.apim_resource_group
 }
