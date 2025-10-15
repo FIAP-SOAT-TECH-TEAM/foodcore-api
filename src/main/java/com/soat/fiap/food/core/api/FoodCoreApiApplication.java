04dc6773b04e7e1ddd76a8408650c8b6ad197fd2
Pedro <ferrarezzolabs@outlook.com>
2025-10-15 16:22:08 -0300

    fix: add @EnableScheduling in ApplicatonContext class

--- src/main/java/com/soat/fiap/food/core/api/FoodCoreApiApplication.java
+++ src/main/java/com/soat/fiap/food/core/api/FoodCoreApiApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication @EnableFeignClients @EnableDiscoveryClient
+@SpringBootApplication @EnableScheduling @EnableFeignClients @EnableDiscoveryClient
 public class FoodCoreApiApplication {
 
 	public static void main(String[] args) {
